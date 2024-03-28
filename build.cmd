9735dceea0ae74ea109fc50af3af6502820942be
Sara Morillon <sara.morillon.pro@gmail.com>
2024-03-28 12:14:07 +0100

    Build modded minecraft

--- build.cmd
+++ build.cmd
@@ -2,3 +2,7 @@
 docker build -t saramorillon/minecraft .
 docker image tag saramorillon/minecraft saramorillon/minecraft:%1
 docker push -a saramorillon/minecraft
+
+docker build -t saramorillon/minecraft-forge -f modded.Dockerfile .
+docker image tag saramorillon/minecraft-forge saramorillon/minecraft-forge:%1
+docker push -a saramorillon/minecraft-forge
